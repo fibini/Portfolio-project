e3956afad066d9f9acfb61969963c36560f1e5ca
Fabien <fabienswift@gmail.com>
2022-05-10 13:30:50 -0400

    Make changes to javascript, use better variable names and css remove hover color gold

--- javascript.js
+++ javascript.js
@@ -1,15 +1,15 @@
-const ham = document.querySelector('.button');
-const navbar = document.querySelector('.nav-bar');
-const clos = document.querySelector('.clos');
+const hamburgerIcon = document.querySelector('.button');
+const navigationBar = document.querySelector('.nav-bar');
+const closeIcon = document.querySelector('.clos');
 
-ham.addEventListener('click', () => {
-  ham.classList.toggle('show');
-  navbar.classList.toggle('show');
-  clos.classList.toggle('show');
+hamburgerIcon.addEventListener('click', () => {
+  hamburgerIcon.classList.toggle('show');
+  navigationBar.classList.toggle('show');
+  closeIcon.classList.toggle('show');
 });
 
 document.querySelectorAll('.adj,.clos').forEach((n) => n.addEventListener('click', () => {
-  ham.classList.remove('show');
-  navbar.classList.remove('show');
-  clos.classList.remove('show');
+  hamburgerIcon.classList.remove('show');
+  navigationBar.classList.remove('show');
+  closeIcon.classList.remove('show');
 }));
