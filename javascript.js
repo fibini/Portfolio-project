40e0172adf318b581f521447d57d2f4aa914aa34
Fabien <fabienswift@gmail.com>
2022-05-10 10:51:07 -0400

    Add index.html, css and javascript for mobile menu to display and hide when interacting with hamburger button

---
+++ javascript.js
@@ -0,0 +1,16 @@
+const ham = document.querySelector(".button");
+const navbar = document.querySelector(".nav-bar");
+const clos = document.querySelector(".clos");
+
+ham.addEventListener("click", () => {
+    ham.classList.toggle("show")
+    navbar.classList.toggle("show")
+    clos.classList.toggle("show")
+})
+
+document.querySelectorAll(".adj,.clos").forEach(n => n.addEventListener("click",() => {
+    ham.classList.remove("show")
+    navbar.classList.remove("show")
+    clos.classList.remove("show")
+
+}))
