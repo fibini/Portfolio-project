b7bfe03261aed592a193f1b0435a30a713efa70c
Fabien <fabienswift@gmail.com>
2022-05-10 12:23:11 -0400

    Add index.html, css and javascript files to show menu display in mobile

--- javascript.js
+++ javascript.js
@@ -1,16 +1,15 @@
-const ham = document.querySelector(".button");
-const navbar = document.querySelector(".nav-bar");
-const clos = document.querySelector(".clos");
+const ham = document.querySelector('.button');
+const navbar = document.querySelector('.nav-bar');
+const clos = document.querySelector('.clos');
 
-ham.addEventListener("click", () => {
-    ham.classList.toggle("show")
-    navbar.classList.toggle("show")
-    clos.classList.toggle("show")
-})
+ham.addEventListener('click', () => {
+  ham.classList.toggle('show');
+  navbar.classList.toggle('show');
+  clos.classList.toggle('show');
+});
 
-document.querySelectorAll(".adj,.clos").forEach(n => n.addEventListener("click", () => {
-    ham.classList.remove("show")
-    navbar.classList.remove("show")
-    clos.classList.remove("show")
-
-}))
+document.querySelectorAll('.adj,.clos').forEach((n) => n.addEventListener('click', () => {
+  ham.classList.remove('show');
+  navbar.classList.remove('show');
+  clos.classList.remove('show');
+}));
